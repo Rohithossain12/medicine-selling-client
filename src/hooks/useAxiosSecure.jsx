3dfe9a619913923d7e857b0c9739ec8492ad83f6
Rohithossain12 <https://github.com/Rohithossain12>
2025-02-19 18:45:51 +0600

    vercel link added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 export const axiosSecure = axios.create({
-  // baseURL: "https://medicine-selling-server-gamma.vercel.app",
-  baseURL:"http://localhost:5000"
+  baseURL: "https://medicine-selling-server-gamma.vercel.app",
+  // baseURL:"http://localhost:5000"
 });
 
 const useAxiosSecure = () => {
